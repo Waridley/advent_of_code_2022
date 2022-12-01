3e15778fbce2b437f3e6566b9c6270b9cf0e1285
Waridley <Waridley64@gmail.com>
2022-12-01 16:49:40 -0600

    Prepare for day 2

---
+++ src/day_2.rs
@@ -0,0 +1,23 @@
+use anyhow::Result;
+
+pub fn eval_part_1(file: &str) -> Result<String> {
+    todo!()
+}
+
+pub fn eval_part_2(file: &str) -> Result<String> {
+    todo!()
+}
+
+#[cfg(test)]
+#[test]
+fn part_1() {
+    let result = eval_part_1("day_2.example").unwrap();
+    assert_eq!(result, "get example answer")
+}
+
+#[cfg(test)]
+#[test]
+fn part_2() {
+    let result = eval_part_2("day_2.example").unwrap();
+    assert_eq!(result, "get example answer")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod day_1;
+pub mod day_2;
 
 pub const INPUT_DIR: &str = "inputs";
